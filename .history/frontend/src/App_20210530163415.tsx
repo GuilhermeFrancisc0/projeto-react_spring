424daf91b4677266a2b82863d5d3881333b26185
Guilherme Francisco <guilhermefranciscoav@gmail.com>
2021-05-30 16:42:20 -0300

    Static charts

---
+++ .history/frontend/src/App_20210530163415.tsx
@@ -0,0 +1,26 @@
+import BarChart from 'components/BarChart';
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <div className="container">
+      <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+      <div className="row px-3">
+        <div className='col-sm-6'>
+          <h5>Todas as Vendas</h5>
+          <BarChart/>
+        </div>
+      </div>
+
+      <DataTable/>
+    </div>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
